559b6dd95780149c90c040361cb17389cdad2718
shresthaaman27@gmail.com <shresthaaman27@gmail.com>
2024-12-30 23:24:34 +0545

    error solved

--- epharm/myapp/models.py
+++ epharm/myapp/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.utils.text import slugify
 
 class Product(models.Model):
-    objects = None
-    id = models.AutoField(primary_key=True)
     CATEGORIES = (
         ('OTC', 'Over-the-Counter'),
         ('RX', 'Prescription Medicines'),
@@ -18,9 +16,7 @@ class Product(models.Model):
 
     generic_name = models.CharField(max_length=200, null=True, blank=True)  # Scientific or generic name
     name = models.CharField(max_length=200, blank=True, null=True)
-
     image = models.ImageField(upload_to='products/', null=True, blank=True)
-
     category = models.CharField(max_length=50, choices=CATEGORIES)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
@@ -30,5 +26,4 @@ class Product(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        # Return the generic name if available, otherwise return the name or a fallback string
         return self.generic_name if self.generic_name else self.name if self.name else "Unnamed Product"
