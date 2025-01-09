b80b1b10b430c11a5d8661b50229f7354e4a4f59
shresthaaman27@gmail.com <shresthaaman27@gmail.com>
2025-01-09 09:49:51 +0545

    login signup first name last name configured

--- epharm/myapp/serializers.py
+++ epharm/myapp/serializers.py
@@ -2,8 +2,9 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
 from rest_framework_simplejwt.tokens import RefreshToken
-from .models import Product
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from .models import Product
+
 User = get_user_model()
 
 # Product Serializer
@@ -16,17 +17,22 @@ class ProductSerializer(serializers.ModelSerializer):
 # User Serializer for creating and managing users (register)
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    first_name = serializers.CharField(required=False, allow_blank=True)  # Optional first name
+    last_name = serializers.CharField(required=False, allow_blank=True)   # Optional last name
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'password', 'first_name', 'last_name']  # Specify fields
 
     def create(self, validated_data):
         # Create user using the validated data
+        print(validated_data)
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
-            password=validated_data['password']
+            password=validated_data['password'],
+            first_name=validated_data.get('first_name', ''),  # Use an empty string if no first name
+            last_name=validated_data.get('last_name', '')     # Use an empty string if no last name
         )
         return user
 
@@ -53,7 +59,7 @@ class UserSerializerWithToken(serializers.ModelSerializer):
         return str(token.access_token)
 
 
-
+# Custom Token Serializer (for login using email instead of username)
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     username = serializers.EmailField(required=True)  # Use email instead of username
 
